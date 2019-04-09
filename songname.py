2c1ea32c0b13c732271d757e6b71f74117ccca80
valkyrie <valkyrie@r3valkyrie.com>
2019-04-09 08:40:47 -0400

    FUCK. YOU. CODACY.

--- songname.py
+++ songname.py
@@ -8,7 +8,7 @@ import urllib.request, json
 def main():
     url = 'https://legacy-api.kexp.org/play/?format=json'
     req = urllib.request.Request(url)
-    with urllib.request.urlopen(req) as url:
+    with urllib.request.urlopen(req) as url: #nosec
         data = json.loads(url.read().decode())
         print(data['results'][0]['artist']['name'] + ' - ' +
                 data['results'][0]['track']['name'])
