64862d9a59984fa2da5df2fa01327a19ccfe2db0
valkyrie <valkyrie@r3valkyrie.com>
2019-04-09 08:08:59 -0400

    Adds IPv4 subnet calculator. Beautifies other scripts.

--- songname.py
+++ songname.py
@@ -4,10 +4,14 @@
 
 import urllib.request, json
 
+
 def main():
-    with urllib.request.urlopen('https://legacy-api.kexp.org/play/?format=json') as url:
+    with urllib.request.urlopen(
+            'https://legacy-api.kexp.org/play/?format=json') as url:
         data = json.loads(url.read().decode())
-        print(data['results'][0]['artist']['name'] + ' - ' + data['results'][0]['track']['name'])
+        print(data['results'][0]['artist']['name'] + ' - ' +
+              data['results'][0]['track']['name'])
+
 
 if __name__ == "__main__":
     main()
