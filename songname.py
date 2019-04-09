992b52bb50566470a67d7810eadab767040510dc
valkyrie <valkyrie@r3valkyrie.com>
2019-04-09 08:38:11 -0400

    Another attempt at auditing urllib

--- songname.py
+++ songname.py
@@ -6,12 +6,13 @@ import urllib.request, json
 
 
 def main():
-    req = urllib.request.Request(
-        'https://legacy-api.kexp.org/play/?format=json')
-    with urllib.request.urlopen(req) as url:
-        data = json.loads(url.read().decode())
-        print(data['results'][0]['artist']['name'] + ' - ' +
-              data['results'][0]['track']['name'])
+    url = 'https://legacy-api.kexp.org/play/?format=json'
+    if url.lower().startswith('https'):
+        req = urllib.request.Request(url)
+        with urllib.request.urlopen(req) as url:
+            data = json.loads(url.read().decode())
+            print(data['results'][0]['artist']['name'] + ' - ' +
+                  data['results'][0]['track']['name'])
 
 
 if __name__ == "__main__":
