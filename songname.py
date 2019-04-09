437374b783f3b26e09ba5620699604e6935b51bf
valkyrie <valkyrie@r3valkyrie.com>
2019-04-09 08:45:49 -0400

    Uses requests library instead of urllib

--- songname.py
+++ songname.py
@@ -2,16 +2,15 @@
 
 # Retrieve's current song and artist name from KEXP, the only radio station I really listen to.
 
-import urllib.request, json
+import requests, json
 
 
 def main():
     url = 'https://legacy-api.kexp.org/play/?format=json'
-    req = urllib.request.Request(url)
-    with urllib.request.urlopen(req) as url: #nosec
-        data = json.loads(url.read().decode())
-        print(data['results'][0]['artist']['name'] + ' - ' +
-                data['results'][0]['track']['name'])
+    r = requests.get(url)
+    data = r.json()
+    print(data['results'][0]['artist']['name'] + ' - ' +
+          data['results'][0]['track']['name'])
 
 
 if __name__ == "__main__":
